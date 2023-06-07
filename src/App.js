859fec17f815e178c6068ff87db9e6a286fea0bd
chaeyoung1027 <rnldyal1027@naver.com>
2023-06-07 09:14:38 +0900

    내용 변경

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Link to="/selectplace" className="title">DotJari.</Link>
         <Link to="/ImaginationCafe" className='selectMenu'> 상상카페 </Link>
         <Link to="/library" className='selectMenu'> 도서관 </Link>
-        <Link to="/selectplace" className='selectMenu'> 예약현황 </Link>  
+        <Link to="/selectplace" className='selectMenu'> 학교 지도 </Link>  
         <a className='Name'>3333 성이름</a>
         <img className='profile' alt="profile" src={Profile} />
       </div>
