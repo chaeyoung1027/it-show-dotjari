1a8d11803e53891c317c933b2de1a17f54438457
chaeyoung1027 <rnldyal1027@naver.com>
2023-06-09 09:39:13 +0900

    라우터 연결 중간커밋

--- src/App.js
+++ src/App.js
@@ -6,28 +6,24 @@ import ImaginationCafe from './components/ImaginationCafe';
 import ReservPlace from './components/ReservPlace';
 import SelectPlace from './components/SelectPlace';
 import Library from './components/Library';
-import Login from './components/Login';
+import Login from './components/login';
 import SignUp from './components/SignUp';
 import UserInfo from './components/UserInfo';
-
 //img
 import Profile from './img/Profile.jpg';
 
 function Layout() {
   return <div>
       <div className='MainTopStyle'>
-        <Link to="/login" className='login'>로그인</Link>
-        <Link to="/signup">회원가입</Link>
-        <Link to="/userinfo">회원정보</Link>
-        <Link to="/selectplace" className="title">DotJari.</Link>
+        <Link to="/places" className="title">DotJari.</Link>
         <Link to="/ImaginationCafe" className='selectMenu'> 상상카페 </Link>
         <Link to="/library" className='selectMenu'> 도서관 </Link>
         <Link to="/selectplace" className='selectMenu'> 학교 지도 </Link>  
         <a className='Name'>3333 성이름</a>
         <img className='profile' alt="profile" src={Profile} />
       </div>
+    <ReservPlace/>
     <Outlet />
-    <ReservPlace />
   </div>
 }
 
