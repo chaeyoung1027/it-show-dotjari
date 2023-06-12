e73171d6e99e026f42532b95e51bca8b94ab6c7d
Chaeyoung Lim <rnldyal1027@naver.com>
2023-06-12 11:23:18 +0900

    개인 페이지 만들기 중

---
+++ src/components/PersonalPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, Outlet } from 'react-router-dom';
+import '../css/PersonalPage.css'
+
+function PersonalPage() {
+  return (
+    <div className="PersonalAll">
+      <div className="sub-title">
+        <h2>예약 현황</h2>
+        <h4 className="Explanation">
+          &nbsp; 예약된 현황을 확인하고, 일정이 변동되었다면 손 쉽게 취소하세요.
+        </h4>
+      </div>
+
+      <div className="presInfo">
+        <div className="position-date">
+          <a className="presPosition">상상카페 B4</a>
+          <a className="presDate">2023.04.05</a>
+        </div>
+          <a className="presTime">2:00~3:00</a>
+      </div>
+    </div>
+  );
+}
+
+export default PersonalPage;
