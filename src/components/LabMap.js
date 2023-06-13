697d2bcade930a26a035d814fc1ce2258f9d1808
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-06-14 08:44:49 +0900

    구글 로그인 기능 추가, 로그인과 회원가입 창 변경

--- src/components/LabMap.js
+++ src/components/LabMap.js
@@ -1,21 +1,15 @@
 import React, { useState } from 'react';
 import '../css/Print.css';
 
+//img
+import Lab from '../img/lab.png';
+
 function PrintLab() {
 
   return (
+    
     <div>
-        <div className='stairs'>계단</div>
-        <div className='room'>응용실습5실</div>
-        <div className='room'>응용실습3실</div>
-        <div className='stairs'>조교실</div>
-        <div className='room'>응용실습1실</div>
-
-        <div className='stairs'>화장실</div>
-        <div className='room'>응용실습4실</div>
-        <div className='stairs'>계단</div>
-        <div className='room'>허브실</div>
-        <div className='room'>응용실습2실</div>
+        <img src={Lab} />
     </div>
   );
 }
