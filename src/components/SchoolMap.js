98d351d0f1221e3dba4bed6319f7bb963923d4ab
steam88ys <yssteam88@gmail.com>
2023-06-14 14:43:40 +0900

    지도 컴포넌트 연결

--- src/components/SchoolMap.js
+++ src/components/SchoolMap.js
@@ -1,29 +1,14 @@
-import './App.css';
 import React from "react";
-import { BrowserRouter as Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function SchoolMap() {
   return (
     <div>
-        <Link to="/placesinfo/lab">응용실습실</Link>
-        <Link to="/placesinfo/imaginationcafe"> 상상카페 </Link>
-        <Link to="/placesinfo/library"> 도서관 </Link>
+        <Link to="/placesinfo/lab">응용실습실</Link><br></br>
+        <Link to="/placesinfo/imaginationcafe">상상카페</Link><br></br>
+        <Link to="/placesinfo/library">도서관</Link>
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/placesinfo" element={<Layout2 />}>
-//           <Route path="imaginationcafe" element={<ImaginationCafeMap />} />
-//           <Route path="library" element={<LibraryMap />} />
-//           <Route path="lab" element={<LabMap />} />
-//         </Route>
-//       </Routes>
-//     </Router>
-//   );
-// }
 
 export default SchoolMap;
