5806c062ce4ced3416b776c370c7a408ac405e5e
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-06-15 20:00:47 +0900

    로그인 시 SelectPlace 화면전환

--- src/components/GoogleButton.js
+++ src/components/GoogleButton.js
@@ -1,28 +1,30 @@
 import React, { useCallback, useEffect, useState } from "react";
 import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
 import { gapi }  from 'gapi-script';
-import SelectPlace from '../components/SelectPlace';
 
 const clientId = '612256972189-4rots1sjfleh5fhfnfh3loihgrpo14iq.apps.googleusercontent.com';
 
-const GoogleButton = ({ onSocial }) => {
+const GoogleButton = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         function start() {
             gapi.client.init({
                 clientId,
-                scope:'email',
+                scope: 'email',
             });
         }
-        
+
         gapi.load('client:auth2', start);
     }, []);
 
     const onSuccess = useCallback((response) => {
         console.log(response);
         setIsLoggedIn(true);
-    }, []);
+        navigate("/places");
+    }, [navigate]);
 
     const onFailure = useCallback((response) => {
         console.log(response);
@@ -31,7 +33,7 @@ const GoogleButton = ({ onSocial }) => {
     return (
         <div>
             {isLoggedIn ? (
-                <SelectPlace />
+                <div>Redirecting...</div>
             ) : (
                 <GoogleLogin
                     clientId={clientId}
@@ -45,3 +47,54 @@ const GoogleButton = ({ onSocial }) => {
 };
 
 export default GoogleButton;
+
+
+
+
+// import React, { useCallback, useEffect, useState } from "react";
+// import GoogleLogin from 'react-google-login';
+// import { gapi }  from 'gapi-script';
+// import { Link } from 'react-router-dom';
+
+// const clientId = '612256972189-4rots1sjfleh5fhfnfh3loihgrpo14iq.apps.googleusercontent.com';
+
+// const GoogleButton = () => {
+//     const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//     useEffect(() => {
+//         function start() {
+//             gapi.client.init({
+//                 clientId,
+//                 scope:'email',
+//             });
+//         }
+        
+//         gapi.load('client:auth2', start);
+//     }, []);
+
+//     const onSuccess = useCallback((response) => {
+//         console.log(response);
+//         setIsLoggedIn(true);
+//     }, []);
+
+//     const onFailure = useCallback((response) => {
+//         console.log(response);
+//     }, []);
+
+//     return (
+//         <div>
+//             {isLoggedIn ? (
+//                 <Link to="/places"></Link>
+//             ) : (
+//                 <GoogleLogin
+//                     clientId={clientId}
+//                     buttonText="구글아이디로 로그인하기"
+//                     onSuccess={onSuccess}
+//                     onFailure={onFailure}
+//                 />
+//             )}
+//         </div>
+//     );
+// };
+
+// export default GoogleButton;
