9efaeb9d0f461009fe47cc7e7b25f215c37c527c
steam88ys <yssteam88@gmail.com>
2023-06-21 11:21:49 +0900

    bar길이 수정

--- src/components/ReservPlace.js
+++ src/components/ReservPlace.js
@@ -81,33 +81,3 @@ const handleCancelReservation = (reservationId) => {
 };
 
 export default ReservPlace;
-
-
-// import { useNavigate, useLocation } from 'react-router-dom';
-// import React from 'react';
-// import '../css/ReservPlace.css';
-
-// function ReservPlace() {
-//   const location = useLocation();
-
-//   const email = location?.state?.email;
-//   console.log(email);
-
-//   return (
-//     <div className='ReservAll'>
-//       <div className="bar"></div>
-//       <div className="reservText">
-//         {email}님,<br />
-//         오늘 <span style={{ color: '#7C00DE' }}>예약된 장소</span>를 확인하세요.
-//       </div>
-//       <div className='resInfo'>
-//         <a className='resPosition'>상상카페 B4</a>
-//         <a className='resTime'>2:00~3:00</a>
-//         <a className='resDate'>2023.04.05</a>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ReservPlace;
-
