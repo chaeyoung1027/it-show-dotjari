dd318f2131a67927451a16bbf026537765965e50
chaeyoung1027 <s2111@e-mirim.hs.kr>
2023-06-22 06:45:53 +0900

    전 좌석 포함해서 예약되는 에러 수정

--- src/components/ImaginationCafe.js
+++ src/components/ImaginationCafe.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import SelectDate from './SelectDate';
 import Seat from './Seat';
 import '../css/ImaginationCafe.css';
 import { MyContext } from '../App';
 import BottomInfo from './BottomInfo';
-import { getFirestore, collection, getDocs,onSnapshot } from "firebase/firestore";
+import { getFirestore, collection, getDocs, onSnapshot } from "firebase/firestore";
 
 /* 상상카페 자리 예약 */
 function ImaginationCafe() {
@@ -36,6 +36,7 @@ function ImaginationCafe() {
   useEffect(() => {
     getReservationData();
   }, []);
+
   useEffect(() => {
     const firestore = getFirestore();
     const reservationRef = collection(firestore, 'reservations');
@@ -66,57 +67,62 @@ function ImaginationCafe() {
     }
   };
 
-  const seatNames = ['A1', 'A2', 'A3', 'A4','A5','A6', 'B1', 'B2', 'B3','B4','B5','B6', 'C1','C2','C3','C4','D1','D2','D3','D4','E1','E2','E3','E4','F1','F2','F3','F4', ' ', ' ', ' ', ' ', ' ', ' ',];
-  const seatsData = [
-    //A
-    {x: 0, y: 0},
-    {x: 190, y: 0},
-    {x: 0, y: 65},
-    {x: 190, y: 65},
-    {x: 0, y: 130},
-    {x: 190, y: 130},
-    //B
-    {x: 0, y: 245},
-    {x: 190, y: 245},
-    {x: 0, y: 310},
-    {x: 190, y: 310},
-    {x: 0, y: 375},
-    {x: 190, y: 375},
-    //C
-    {x: 350, y: 65},
-    {x: 540, y: 65},
-    {x: 350, y: 130},
-    {x: 540, y: 130},
-    //D
-    {x: 350, y: 245},
-    {x: 540, y: 245},
-    {x: 350, y: 310},
-    {x: 540, y: 310},
-    //E
-    {x: 700, y: 65},
-    {x: 890, y: 65},
-    {x: 700, y: 130},
-    {x: 890, y: 130},
-    //F
-    {x: 700, y: 245},
-    {x: 890, y: 245},
-    {x: 700, y: 310},
-    {x: 890, y: 310},
-    //테이블
-    {x: 70, y: 5, width : 105, height : 175},
-    {x: 70, y: 250, width : 105, height : 175},
-    {x: 420, y: 68, width : 105, height : 110},
-    {x: 420, y: 250, width : 105, height : 110},
-    {x: 770, y: 68, width : 105, height : 110},
-    {x: 770, y: 250, width : 105, height : 110},
-  ];
-
+  // 예약 완료 후 selectedSeats 초기화
   const handleReservation = () => {
     const reservationData = {
       selectedSeats: selectedSeats
-    }
+    };
+
+    // 예약이 완료된 후에 selectedSeats를 빈 배열로 설정
+    setSelectedSeats([]);
+
     return reservationData;
-  }
+  };
+
+  const seatNames = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'E4', 'F1', 'F2', 'F3', 'F4', ' ', ' ', ' ', ' ', ' ', ' ',];
+  const seatsData = [
+    //A
+    { x: 0, y: 0 },
+    { x: 190, y: 0 },
+    { x: 0, y: 65 },
+    { x: 190, y: 65 },
+    { x: 0, y: 130 },
+    { x: 190, y: 130 },
+    //B
+    { x: 0, y: 245 },
+    { x: 190, y: 245 },
+    { x: 0, y: 310 },
+    { x: 190, y: 310 },
+    { x: 0, y: 375 },
+    { x: 190, y: 375 },
+    //C
+    { x: 350, y: 65 },
+    { x: 540, y: 65 },
+    { x: 350, y: 130 },
+    { x: 540, y: 130 },
+    //D
+    { x: 350, y: 245 },
+    { x: 540, y: 245 },
+    { x: 350, y: 310 },
+    { x: 540, y: 310 },
+    //E
+    { x: 700, y: 65 },
+    { x: 890, y: 65 },
+    { x: 700, y: 130 },
+    { x: 890, y: 130 },
+    //F
+    { x: 700, y: 245 },
+    { x: 890, y: 245 },
+    { x: 700, y: 310 },
+    { x: 890, y: 310 },
+    //테이블
+    { x: 70, y: 5, width: 105, height: 175 },
+    { x: 70, y: 250, width: 105, height: 175 },
+    { x: 420, y: 68, width: 105, height: 110 },
+    { x: 420, y: 250, width: 105, height: 110 },
+    { x: 770, y: 68, width: 105, height: 110 },
+    { x: 770, y: 250, width: 105, height: 110 },
+  ];
 
   const screenSize = { width: 500, height: 600 }; // 나중에 조절하기 margin같은거
   return (
@@ -161,7 +167,7 @@ function ImaginationCafe() {
 
           // Check if the seat is reserved
           const reservedSeat = reservation.find((reservationData) => reservationData.componentOption === 0 && reservationData.seat === seatName &&
-          selectedDate == reservationData.selectedDay
+            selectedDate == reservationData.selectedDay
           //시간 조건 넣기
           );
           if (reservedSeat) {
