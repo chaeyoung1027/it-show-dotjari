e267292ec0f7f48a8e9c8452c12c47cdb4cc667c
chaeyoung1027 <s2111@e-mirim.hs.kr>
2023-06-22 13:15:37 +0900

    조건 추가

--- src/components/BottomInfo.js
+++ src/components/BottomInfo.js
@@ -22,6 +22,20 @@ function BottomInfo({ onReservation, currentComponent , selectedSeats}) {
   const handleReserveClick = () => {
     const reservationData = onReservation();
     let ComponentOption = 0;
+    const currentDate = new Date();
+  const currentYear = currentDate.getFullYear(); // Get the current year
+  const currentMonth = currentDate.getMonth() + 1; // Get the current month (add 1 because months are zero-based)
+  const currentDay = currentDate.getDate(); // Get the current day
+
+  const dateString = selectedDate;
+  const dateComponents = dateString.split(" ");
+
+  const year = parseInt(dateComponents[0]);
+  const month = parseInt(dateComponents[1]);
+  const day = parseInt(dateComponents[2]);
+
+  const isToday = year === currentYear && month === currentMonth && day === currentDay;
+
 
     if (currentComponent === 'ImaginationCafe') {
       ComponentOption = 0;
@@ -35,7 +49,7 @@ function BottomInfo({ onReservation, currentComponent , selectedSeats}) {
         position: toast.POSITION.TOP_RIGHT
       });
     }
-    if(parseInt(selectedTime)<currentHour||(selectedTime==currentHour && parseInt(selectedMinute)<currentMinute)){
+    if((isToday)&&parseInt(selectedTime)<currentHour||(selectedTime==currentHour && parseInt(selectedMinute)<currentMinute)){
       toast.success('이미 지난 시간입니다! \n 시간을 다시 확인해주세요', {
         position: toast.POSITION.TOP_RIGHT
       });
@@ -45,7 +59,7 @@ function BottomInfo({ onReservation, currentComponent , selectedSeats}) {
         position: toast.POSITION.TOP_RIGHT
       });
     }
-    else if(selectedSeats==undefined){
+    else if(selectedSeats==''){
       toast.success('좌석을 선택해주세요', {  //TODO : 좌석이 선택이 안되어있으면 띄우기
         position: toast.POSITION.TOP_RIGHT
       });
@@ -64,6 +78,7 @@ function BottomInfo({ onReservation, currentComponent , selectedSeats}) {
         );
       });
 
+      console.log(selectedSeats)
       // Display the toast message in the top-right corner
       toast.success('예약이 완료되었습니다.', {
         position: toast.POSITION.TOP_LEFT
